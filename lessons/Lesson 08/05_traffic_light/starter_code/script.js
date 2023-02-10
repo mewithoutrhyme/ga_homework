c04ba7c9b758910a1565f71ea9df18ed63fe9815
mewithoutrhyme <me@withoutrhyme.com>
2023-02-09 21:07:36 -0500

    classwork - 8

--- lessons/Lesson 08/05_traffic_light/starter_code/script.js
+++ lessons/Lesson 08/05_traffic_light/starter_code/script.js
@@ -1,11 +1,3 @@
 // Try making the traffic light work - Think through the problem BEFORE you code!
 // You'll use addEventListener and getElementById if you're doing it right...
 
-function stopLight ( ButtonID, SelectorID ) {
-    var stoplightButton = document.getElementById(ButtonID);
-    var stopLightSelector = document.getElementById(SelectorID);
-    stopLightButton.addEventListener("click", function(){
-   stopLightSelector.classList.BackgroundColor("red");
-    });
-    }
-    stopLight( "stopButton" , "stopLight" );
