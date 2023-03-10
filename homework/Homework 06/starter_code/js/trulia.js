1d34cda02bd00c36ba1983379e5417e036b6bb53
mewithoutrhyme <me@withoutrhyme.com>
2023-03-10 09:26:52 -0500

    homework 6 final saves

--- homework/Homework 06/starter_code/js/trulia.js
+++ homework/Homework 06/starter_code/js/trulia.js
@@ -5,10 +5,10 @@
 
   document.querySelector(".trulia-nav-toggle").addEventListener('click',function(e){
     e.preventDefault();
-    let navSet = document.querySelectorAll('.trulia-nav > nav ul');
+    let navSwitch = document.querySelectorAll('.trulia-nav > nav ul');
 
-    for(let n=0;n<navSet.length;n++){
-        navSet[n].classList.toggle('trulia-nav-mobilehide');
+    for (let n = 0; n < navSwitch.length; n++){
+      navSwitch[n].classList.toggle('trulia-nav-mobilehide');
     }
 
 });
