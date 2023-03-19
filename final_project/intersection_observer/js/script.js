b663bb5a2c90eab18758c3644ab09118e17ca91b
mewithoutrhyme <me@withoutrhyme.com>
2023-03-19 09:39:28 -0400

    final project

--- final_project/intersection_observer/js/script.js
+++ final_project/intersection_observer/js/script.js
@@ -12,3 +12,36 @@ document.addEventListener('DOMContentLoaded', function(event) {
     });
 
 });
+
+// fade and scroll items into view while scrolling
+
+const faders = document.querySelectorAll(".fade-in");
+const sliders = document.querySelectorAll(".slide-in");
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -250px 0px"
+  };
+
+const appearOnScroll = new IntersectionObserver(function(
+    entries,
+    appearOnScroll
+  ) {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) {
+        return;
+      } else {
+        entry.target.classList.add("appear");
+        appearOnScroll.unobserve(entry.target);
+      }
+    });
+  },
+  appearOptions);
+  
+  faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+  });
+  
+  sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+  });
